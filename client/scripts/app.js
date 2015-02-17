d39ea94810541944f4340b6654b9b051c35dfe13
wcwung <williamcwung@gmail.com>
2015-02-16 19:41:09 -0800

    Add POST function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,80 @@
+$(function(){
 // YOUR CODE HERE:
 // var message = "This is a test. Of REST. The best."
 
-// $.ajax({
-//   // always use this url
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
+// var postMessages = function() {
+//   $.ajax({
+//     // always use this url
+//     url: 'https://api.parse.com/1/classes/chatterbox',
+//     type: 'POST',
+//     data: JSON.stringify(message),
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log('chatterbox: Message sent');
+//     },
+//     error: function (data) {
+//       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//       console.error('chatterbox: Failed to send message');
+//     }
+//   });
+// };
 
-var pullMessages = function(){
+var app = {};
+app.init = function(){};
+app.send = function(message){
+  var dataMessage = {
+    username : window.location.search.slice(10),
+    text : message,
+    roomname : undefined
+  };
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(dataMessage),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+app.fetch = function(){
 
   var messageIds = [];
 
   setInterval(function(){
-    $.get ("https://api.parse.com/1/classes/chatterbox?order=-createdAt", function(data) {
-      for (var i = data.results.length-1; i >= 0; i--){
-        if (!_.contains( messageIds, data.results[i].objectId )){
-          var msg = $("<div/>").text(data.results[i].text).html();
-          var user = $("<div/>").text(data.results[i].username).html();
-          $(".messages").prepend("<li>" + user + ": " + msg + data.results[i].createdAt + "</li>");
-
-          messageIds.push(data.results[i].objectId);
+    $.ajax({
+        // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        for (var i = data.results.length-1; i >= 0; i--){
+          if (!_.contains( messageIds, data.results[i].objectId )){
+            var msg = $("<div/>").text(data.results[i].text).html();
+            var user = $("<div/>").text(data.results[i].username).html();
+            $(".messages").prepend("<li>" + "<span class='user'>" + user + "</span>" + ": " + "<span class='message'>" + msg + "</span>" + "</li>");
+            messageIds.push(data.results[i].objectId);
+          }
         }
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get messages.');
       }
     });
   }, 500);
 };
 
-pullMessages();
+app.fetch();
+
+$('.submit-btn').on('click', function() {
+  app.send($(".submission-input").val());
+});
 
 // $('.messages').append(message);
+});
